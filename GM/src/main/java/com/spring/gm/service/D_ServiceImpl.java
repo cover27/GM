e8f65dabfaef9ba570bd4969f0acdc163d127d12
kosmo <kosmo@DESKTOP-3EHK6JM>
2019-02-22 20:25:13 +0900

    장훈

--- GM/src/main/java/com/spring/gm/service/D_ServiceImpl.java
+++ GM/src/main/java/com/spring/gm/service/D_ServiceImpl.java
@@ -88,9 +88,13 @@ public class D_ServiceImpl implements D_Service{
 		int endPage = 0;		// 마지막 페이지		
 		
 		int num = Integer.parseInt(req.getParameter("num"));
-		
 		int company = ((MemberVO)req.getSession().getAttribute("loginInfo")).getCompany();
-		cnt = dao.getBoardArticleCnt(company);
+		
+		Map<String, Object> cntMap = new HashMap<String, Object>();
+		cntMap.put("num", num);
+		cntMap.put("company", company);
+		
+		cnt = dao.getUserBoardArticleCnt(cntMap);
 		
 		pageNum = req.getParameter("pageNum");
 		
